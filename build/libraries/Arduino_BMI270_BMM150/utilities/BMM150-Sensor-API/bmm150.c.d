2fe004509aff212116ceefed49615081f095f67a
zjoseph03 <zachjoseph@hotmail.com>
2025-01-30 17:58:58 -0800

    updated gitignore

---
+++ build/libraries/Arduino_BMI270_BMM150/utilities/BMM150-Sensor-API/bmm150.c.d
@@ -0,0 +1,4 @@
+d:\Courses\ELEC391\build\libraries\Arduino_BMI270_BMM150\utilities\BMM150-Sensor-API\bmm150.c.o: \
+ C:\Users\zachj\OneDrive\ -\ UBC\Documents\Arduino\libraries\Arduino_BMI270_BMM150\src\utilities\BMM150-Sensor-API\bmm150.c \
+ C:\Users\zachj\OneDrive\ -\ UBC\Documents\Arduino\libraries\Arduino_BMI270_BMM150\src\utilities\BMM150-Sensor-API\bmm150.h \
+ C:\Users\zachj\OneDrive\ -\ UBC\Documents\Arduino\libraries\Arduino_BMI270_BMM150\src\utilities\BMM150-Sensor-API\bmm150_defs.h
