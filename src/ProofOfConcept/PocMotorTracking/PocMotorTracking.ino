d781ddbbf9887d062659bce9c0ec166ee11fbf06
zjoseph03 <zachjoseph@hotmail.com>
2025-02-12 12:56:09 -0800

    still not working:

--- src/ProofOfConcept/PocMotorTracking/PocMotorTracking.ino
+++ src/ProofOfConcept/PocMotorTracking/PocMotorTracking.ino
@@ -4,10 +4,10 @@
 
 
 
-PWMController pwmController(2); // motor 1 forward
-PWMController pwmController2(3); // motor 1 backward
-PWMController pwmController3(4); // motor 2 forward
-PWMController pwmController4(5); // motor 2 backward
+// PWMController pwmController(2); // motor 1 forward
+// PWMController pwmController2(3); // motor 1 backward
+// PWMController pwmController3(4); // motor 2 forward
+// PWMController pwmController4(5); // motor 2 backward
 
 void setup() {
   Serial.begin(9600);
@@ -16,11 +16,16 @@ void setup() {
     while (1);  // Stop if IMU initialization fails
   }
   previousTime = millis();
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+
   // NOTE: WRITE DEFAULT FOR THIS!!!!
-  pwmController.setFrequency(500);  // Change to 500Hz
-  pwmController2.setFrequency(500);
-  pwmController3.setFrequency(500);  // Change to 500Hz
-  pwmController4.setFrequency(500);
+  // pwmController.setFrequency(500);  // Change to 500Hz
+  // pwmController2.setFrequency(500);
+  // pwmController3.setFrequency(500);  // Change to 500Hz
+  // pwmController4.setFrequency(500);
 }
 
 void loop() {
@@ -62,10 +67,14 @@ void loop() {
     // Roll = 30 degrees means move motors forward 100%
     dutyCycle = (roll * (100.0 / 30.0));
     dutyCycleBackwards = 0.0;
-    pwmController.writePWMDutyCycle(100);
-    pwmController2.writePWMDutyCycle(dutyCycleBackwards); 
-    pwmController3.writePWMDutyCycle(100);
-    pwmController4.writePWMDutyCycle(dutyCycleBackwards);
+    analogWrite(2, 255);
+    analogWrite(3, 0);
+    analogWrite(4, 255);
+    analogWrite(5, 0);
+    // pwmController.writePWMDutyCycle(100);
+    // pwmController2.writePWMDutyCycle(dutyCycleBackwards); 
+    // pwmController3.writePWMDutyCycle(100);
+    // pwmController4.writePWMDutyCycle(dutyCycleBackwards);
     Serial.print("Motor 1 Forward: ");
     Serial.println(dutyCycle, 6);
     Serial.print("Motor 1 Backward: ");
@@ -81,10 +90,14 @@ void loop() {
     dutyCycleBackwards = (roll * (100.0 / 30.0));
     dutyCycle = 0.0;
 
-    pwmController.writePWMDutyCycle(dutyCycle);
-    pwmController2.writePWMDutyCycle(100);
-    pwmController3.writePWMDutyCycle(dutyCycle);
-    pwmController4.writePWMDutyCycle(100);
+    analogWrite(2, 0);
+    analogWrite(3, 255);
+    analogWrite(4, 0);
+    analogWrite(5, 255);
+    // pwmController.writePWMDutyCycle(dutyCycle);
+    // pwmController2.writePWMDutyCycle(100);
+    // pwmController3.writePWMDutyCycle(dutyCycle);
+    // pwmController4.writePWMDutyCycle(100);
 
 
     Serial.print("Motor 1 Forward: ");
@@ -100,10 +113,14 @@ void loop() {
     dutyCycle = 0.0;
 
     Serial.println("NO MOVEMENT");
-    pwmController.writePWMDutyCycle(0);
-    pwmController2.writePWMDutyCycle(0);
-    pwmController3.writePWMDutyCycle(0);
-    pwmController4.writePWMDutyCycle(0);
+    analogWrite(2, 0);
+    analogWrite(3, 0);
+    analogWrite(4, 0);
+    analogWrite(5, 0);
+    // pwmController.writePWMDutyCycle(0);
+    // pwmController2.writePWMDutyCycle(0);
+    // pwmController3.writePWMDutyCycle(0);
+    // pwmController4.writePWMDutyCycle(0);
 
     Serial.print("Motor 1 Forward: ");
     Serial.println(dutyCycle, 6);
